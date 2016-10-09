ebc9bd6994337424782676631ea0660b8b210dbb
impaler <impaler777@gmail.com>
2016-10-09 20:41:20 +1100

    (fix) Block-scoped let in deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,7 +13,7 @@ if (/dev|master|deploy-test/.test(TRAVIS_BRANCH)) {
         const DEPLOY_USER = process.env['DEPLOY_USER']
         const RSYNC_COMMAND = `rsync -e "ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/${KEY_NAME} -p ${DEPLOY_PORT}" --delete -avr ${TRAVIS_BUILD_DIR}/${BUILD_FOLDER} ${DEPLOY_USER}@${DEPLOY_HOST}:${TRAVIS_BRANCH}`
 
-        let command = execSync(RSYNC_COMMAND)
+        var command = execSync(RSYNC_COMMAND)
         console.log(`rsync command completed with ${command}`)
         process.exit(0)
 
